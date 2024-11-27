18f683cad60f1baa9ec83a250307a4cce4274374
Lay <layssa.lima166@gmail.com>
2024-11-27 08:48:59 -0300

    feat: using router hook

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,10 +1,17 @@
-import Link from "next/link";
+"use client";
+import { useRouter } from "next/navigation";
 
 export default function Login() {
+  const router = useRouter();
+
+  function navigateToPlataform() {
+    router.push("/platform");
+  }
+
   return (
     <div>
       <p>login</p>
-      <Link href="/platform">Entrar</Link>
+      <button onClick={navigateToPlataform}>Entrar</button>
     </div>
   );
 }
